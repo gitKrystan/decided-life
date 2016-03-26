b084a71fe9a91793f9e08a736cefc196d3183c95
Krystan Menne <kmenne@gmail.com>
2016-03-25 19:20:10 -0700

    test that authenticate_user is working

--- spec/features/user_authentication_pages_spec.rb
+++ spec/features/user_authentication_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'the sign up path' do
-  it 'allows a user to sign up' do
+feature 'the sign up path' do
+  scenario 'allows a user to sign up' do
     new_user = build :user
     visit root_path
     click_link 'Sign Up'
@@ -14,11 +14,18 @@ describe 'the sign up path' do
   end
 end
 
-describe 'the user authentication path' do
-  it 'allows a user to sign in' do
+feature 'the user authentication path' do
+  scenario 'allows a user to sign in' do
     user = create :confirmed_user
     login_as(user)
     visit root_path
     expect(page).to have_content(user.name)
+  end
+end
+
+feature 'validates user before managing records' do
+  scenario 'does not allow someone who is not logged in to create a matrix' do
+    visit new_matrix_path
+    expect(page).to have_content('sign in or sign up before continuing')
   end
 end
