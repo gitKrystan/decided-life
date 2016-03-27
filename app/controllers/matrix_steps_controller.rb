89444518da7b9b43ffc7cff0ccefc10196a105d4
Krystan Menne <kmenne@gmail.com>
2016-03-27 13:23:40 -0700

    refactor matrix creation to wizard form that also creates criteria and options

---
+++ app/controllers/matrix_steps_controller.rb
@@ -0,0 +1,39 @@
+class MatrixStepsController < MatricesChildrenController
+  include Wicked::Wizard
+  steps :criteria, :options
+
+  def show
+    case step
+    when :criteria
+      1.times { @matrix.criteria.build }
+      @criteria = @matrix.criteria
+    when :options
+      1.times { @matrix.options.build }
+      @options = @matrix.options
+    end
+    render_wizard
+  end
+
+  def update
+    case step
+    when :criteria
+      flash[:notice] = 'Your criteria have been updated.'
+    when :options
+      flash[:notice] = 'Your options have been updated.'
+    end
+    @matrix.update(matrix_params)
+    render_wizard @matrix
+  end
+
+  private
+
+  def matrix_params
+    params.require(:matrix).permit(:name,
+                                   criteria_attributes: [:id, :name, :weight],
+                                   options_attributes: [:id, :name])
+  end
+
+  def finish_wizard_path
+    @matrix
+  end
+end
