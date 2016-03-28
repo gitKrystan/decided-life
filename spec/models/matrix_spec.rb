c8dc2e0dd6e46338cbf81e0cf981e55da5f93c52
Krystan Menne <kmenne@gmail.com>
2016-03-27 20:33:22 -0700

    test matrix reject_if for criterium nested attributes

--- spec/models/matrix_spec.rb
+++ spec/models/matrix_spec.rb
@@ -6,6 +6,19 @@ RSpec.describe Matrix, type: :model do
 
   it { should have_many(:criteria).dependent :destroy }
   it { should accept_nested_attributes_for(:criteria).allow_destroy(true) }
+  it 'rejects attributes for a criterium if they are blank or default' do
+    test_criterium_attributes = { name: '', weight: '100' }
+    test_matrix = create :matrix
+    test_matrix.update(criteria_attributes: [test_criterium_attributes])
+    expect(test_matrix.criteria.count).to equal 0
+  end
+
+  it 'creates a criterium with valid attributes' do
+    test_criterium_attributes = attributes_for :criterium
+    test_matrix = create :matrix
+    test_matrix.update(criteria_attributes: [test_criterium_attributes])
+    expect(test_matrix.criteria.count).to equal 1
+  end
 
   it { should have_many(:options).dependent :destroy }
   it { should accept_nested_attributes_for(:options).allow_destroy(true) }
