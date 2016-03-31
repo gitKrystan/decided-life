0e3414822f3f797bee196554096571fb771e3958
Krystan Menne <kmenne@gmail.com>
2016-03-31 14:45:04 -0700

    add rating to scores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327162024) do
+ActiveRecord::Schema.define(version: 20160331212356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20160327162024) do
   create_table "scores", force: :cascade do |t|
     t.integer  "criterium_id"
     t.integer  "option_id"
-    t.integer  "score",        default: 0
+    t.integer  "amount",       default: 0
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Score, type: :model do
   it { should belong_to :criterium }
   it { should belong_to :option }
+  it { should validate_presence_of :amount }
 end
