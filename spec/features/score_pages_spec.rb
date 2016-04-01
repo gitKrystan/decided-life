df5b1761f29a3ccdfd5ae14385e768c6bb583f73
Krystan Menne <kmenne@gmail.com>
2016-04-01 13:46:46 -0700

    submit score form on blur of input

--- spec/features/score_pages_spec.rb
+++ spec/features/score_pages_spec.rb
@@ -15,6 +15,13 @@ feature 'Scoring a Matrix:' do
   scenario 'Adds a score to a criterium/option combination', js: true do
     visit edit_matrix_path(test_matrix)
     fill_in "score_amount_#{test_criterium.id}_#{test_option.id}", with: '42'
+    page.find('body').click
+    expect(page).to have_selector("input[value='42']")
+  end
+
+  scenario 'Adds a score to a criterium/option combination without js' do
+    visit edit_matrix_path(test_matrix)
+    fill_in "score_amount_#{test_criterium.id}_#{test_option.id}", with: '42'
     click_button "save_score_#{test_criterium.id}_#{test_option.id}"
     expect(page).to have_selector("input[value='42']")
   end
@@ -23,13 +30,6 @@ feature 'Scoring a Matrix:' do
     test_score = create :score, criterium: test_criterium, option: test_option
     visit edit_matrix_path(test_matrix)
     expect(page).to have_selector("input[value='#{test_score.amount}']")
-    fill_in "score_amount_#{test_criterium.id}_#{test_option.id}", with: '42'
-    click_button "save_score_#{test_criterium.id}_#{test_option.id}"
-    expect(page).to have_selector("input[value='42']")
-  end
-
-  scenario 'Adds a score to a criterium/option combination without js' do
-    visit edit_matrix_path(test_matrix)
     fill_in "score_amount_#{test_criterium.id}_#{test_option.id}", with: '42'
     click_button "save_score_#{test_criterium.id}_#{test_option.id}"
     expect(page).to have_selector("input[value='42']")
