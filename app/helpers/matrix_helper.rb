4a870fa9c900e83830e6ddfdc640789f7e7ed4e6
Krystan Menne <kmenne@gmail.com>
2016-04-07 15:32:24 -0700

    display sort direction to user

--- app/helpers/matrix_helper.rb
+++ app/helpers/matrix_helper.rb
@@ -1,13 +1,14 @@
 module MatrixHelper
   def sort_options(sort_method, title = nil)
     title ||= sort_method.titleize
-    method_is_current_method = sort_method == options_sort_method
-    direction_is_ascending = options_sort_direction == 'asc'
-    direction = if method_is_current_method && direction_is_ascending
+    method_is_current = sort_method == options_sort_method
+    css_class = method_is_current ? "dec-#{options_sort_direction}" : nil
+    direction = if method_is_current && (options_sort_direction == 'asc')
                   'desc'
                 else
                   'asc'
                 end
-    link_to title, sort_options: sort_method, options_direction: direction
+    link_to title, { sort_options: sort_method, options_direction: direction },
+            class: css_class
   end
 end
