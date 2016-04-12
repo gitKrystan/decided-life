7b6732a53c4279e5b9e996f591b407c90e64677d
Krystan Menne <kmenne@gmail.com>
2016-04-12 08:34:24 -0700

    destroy scores when bins are destroyed

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,6 +1,6 @@
 class Bin < ActiveRecord::Base
   belongs_to :criterium
-  has_many :scores
+  has_many :scores, dependent: :destroy
 
   def select_description
     score_description = ": #{description}" unless description.empty?
--- spec/models/bin_spec.rb
+++ spec/models/bin_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Bin, type: :model do
   it { should belong_to :criterium }
-  it { should have_many :scores }
+  it { should have_many(:scores).dependent :destroy }
 end
